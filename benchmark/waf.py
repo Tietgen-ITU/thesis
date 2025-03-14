870b6ef8f0067b8ce159e9be9579a40ceb7ad190
emiloh <82398520+emiloh@users.noreply.github.com>
2025-03-14 11:21:06 +0100

    fix: update waf script to only use nvme cli

--- benchmark/waf.py
+++ benchmark/waf.py
@@ -16,32 +16,12 @@ written_offset = list(map(int, os.getenv("WRITTEN_OFFSET").split("-")))
 measurement_interval = 900
 measurement_duration = 24 # every 15 min for 6 hours
 
-def get_waf_non_fdp(last_host, last_media):
+def get_waf(last_host, last_media):
     cmd = f"""{nvme} get-log {device} --log-id={id_log} --log-len=512 -b"""
     res = subprocess.check_output(cmd, shell=True)
     host = int.from_bytes(res[sent_offset[0]:sent_offset[1]+1], byteorder="little") 
     media = int.from_bytes(res[written_offset[0]:written_offset[1]+1], byteorder="little") 
 
-    if host == 0: return (0,0,0)
-    
-    diff_host = host - last_host
-    diff_media = media - last_media
-
-    return (diff_media/diff_host, host, media)
-
-def get_waf_fdp(last_host, last_media):
-    cmd = f"""{xnvme} log-fdp-stats {device} --lsi {id_log_xnvme}"""
-    res = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode("utf-8")
-    host = 0
-    media = 0
-
-    for i, line in enumerate(res.split('\n')):
-        if i == 3:
-            num = re.search(r"""\d+""", line).group()
-            host = int(num) if num else 0
-        elif i == 4:
-            num = re.search(r"""\d+""", line).group()
-            media = int(num) if num else 0
     if host == 0: return (0,0,0)
     
     diff_host = host - last_host
@@ -59,7 +39,7 @@ def measure_waf(out, fdp):
     waf_file = open(out, "w+")
     while points < max_points:
         time.sleep(measurement_interval)
-        waf_stats = get_waf_fdp(current_host, current_media) if fdp else get_waf_non_fdp(current_host, current_media)
+        waf_stats = get_waf(current_host, current_media)
         current_host = waf_stats[1]
         current_media = waf_stats[2]
         waf_file.write(f"{datetime.now()} - {waf_stats}\n")
@@ -70,7 +50,4 @@ def measure_waf(out, fdp):
 
 if __name__ == "__main__":
     out = sys.argv[1]
-    fdp = False
-    if sys.argv[2] == "true":
-        fdp = True
-    measure_waf(out, fdp)
+    measure_waf(out)
