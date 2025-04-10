6d5415722e80741ffa043ba8d512c515d3389844
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-10 14:01:47 +0000

    Add a wrapper function that controls the duration of the benchmark

--- benchmark/duckdb/runner/benchmark_types.py
+++ benchmark/duckdb/runner/benchmark_types.py
@@ -2,5 +2,6 @@ from typing import Callable
 
 from database.duckdb import Database
 
-type BenchmarkRunnerFunc = Callable[[Database], None]
+type BenchmarkRunnerFunc = Callable[[Database, int], list[str]]
+type BenchmarkEpocFunc = Callable[[Database], list[str]]
 type BenchmarkSetupFunc = Callable[[Database], None]
