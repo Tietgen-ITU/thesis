ef7003532c54fdd00cc54d32abc21cc1a7ff2293
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-10 06:49:07 +0000

    Uncomment setup of benchmarks

--- benchmark/duckdb/benchmark.py
+++ benchmark/duckdb/benchmark.py
@@ -90,7 +90,7 @@ class Arguments:
         return arguments
 
 type SetupFunc = Callable[[int], duckdb.Database]
-def prepare_setup_func(args: Arguments) -> tuple[SetupFunc, SetupFunc]:
+def prepare_setup_func(args: Arguments) -> SetupFunc:
     """
     Prepare the database configuration and database extensions that are needed depending on the storage device
     """
@@ -101,11 +101,22 @@ def prepare_setup_func(args: Arguments) -> tuple[SetupFunc, SetupFunc]:
         nvme_device_path, generic_device_path = setup_device(device, enable_fdp=args.use_fdp)
         device_path = generic_device_path if args.use_generic_device else nvme_device_path
 
+        # Ensure that the extension is loaded and the
+        con = duckdb.connect(config={"allow_unsigned_extensions": "true"})
+        con.load_extension("nvmefs")
+        con.execute(f"""CREATE OR REPLACE PERSISTENT SECRET nvmefs (
+                        TYPE NVMEFS,
+                        nvme_device_path '{device.device_path}',
+                        fdp_plhdls       '{7}'
+                    );""")
+    
+        con.execute("ATTACH DATABASE 'nvmefs:///bench.db' AS bench (READ_WRITE);")
+        con.close()
+
         config = duckdb.ConnectionConfig(
             device_path, 
             args.io_backend, 
             args.use_fdp)
-
         db = duckdb.connect("nvmefs:///bench.db", config)
         db.query(f"SET memory_limit='{buffer_manager_size}MB';")
 
@@ -118,23 +129,23 @@ def prepare_setup_func(args: Arguments) -> tuple[SetupFunc, SetupFunc]:
 
         return db
 
-    return setup_normal, setup_nvme
+    return setup_nvme if args.device is not None else setup_normal
 
 
 if __name__ == "__main__":
 
     args: Arguments = Arguments.parse_args()
-    setup_normal, setup_nvme = prepare_setup_func(args)
+    setup_database = prepare_setup_func(args)
 
-    # run_benchmark, setup_benchmark = create_benchmark_runner(args.benchmark)
+    run_benchmark, setup_benchmark = create_benchmark_runner(args.benchmark)
 
-    # for i in range(args.iterations):
-    #     buffer_manager_mem_size = 1024 # TODO: This could be something that is a paramter for our setup
+    for i in range(args.iterations): # TODO: This should maybe just be moved to the benchmark runner?
+        buffer_manager_mem_size = 1024 # TODO: This could be something that is a paramter for our setup
 
-    #     # Setup the database with the correct device config
-    #     db = setup_nvme(buffer_manager_mem_size) if args.device is not None else setup_normal(buffer_manager_mem_size) 
+        # Setup the database with the correct device config
+        db: duckdb.Database = setup_database(buffer_manager_mem_size)
 
-    #     setup_benchmark(db)
+        setup_benchmark(db)
 
     #     # NOTE: The connection is not thread-safe, search for duckdb cursor in the client library to see how to use in a multi-threaded environment
     #     run_benchmark(db)
