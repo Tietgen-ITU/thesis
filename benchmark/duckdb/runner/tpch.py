970fafd1c7f445efd0408e771258578e1a6600e9
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-10 06:50:06 +0000

    Add setup of TPCH

--- benchmark/duckdb/runner/tpch.py
+++ benchmark/duckdb/runner/tpch.py
@@ -4,11 +4,22 @@ TPCH_BENCHMARK_NAME = "name"
 
 def setup_tpch_benchmark(db: Database):
     # TODO: Load the tpch extension to be used when running the benchmarks
+    db.load_extension("tpch")
 
-    # TODO: Query if the table already exists then there is no reason to copy it over to the new db
+    db.query("ATTACH DATABASE 'tpch-1.db' AS tpch (READ_WRITE);") # TODO: Add parameter with file to copy data from
+    db.query("COPY FROM DATABASE tpch TO bench;")
+    db.query("DETACH DATABASE tpch;")
+
+    db.query("USE bench;")
+
     pass
 
 def run_tpch_benchmark(db: Database):
-    # TODO: Implement TPCH benchmark
-    print("Running TPCH benchmark(not implemented yet)")
+
+    for query_nr in range(1, 22):
+        result = db.query(f"PRAGMA tpch({query_nr});")
+
+        # TODO: Can we add query result verification???
+
+
     pass
