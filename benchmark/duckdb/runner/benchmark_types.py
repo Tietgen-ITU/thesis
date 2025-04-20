c130cbff381c2fe2d4f3ccfbdea930c139725719
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-20 09:50:27 +0000

    Add and 'h' to epoch

--- benchmark/duckdb/runner/benchmark_types.py
+++ benchmark/duckdb/runner/benchmark_types.py
@@ -3,5 +3,5 @@ from typing import Callable
 from database.duckdb import Database
 
 type BenchmarkRunnerFunc = Callable[[Database, int], list[str]]
-type BenchmarkEpocFunc = Callable[[Database], list[str]]
+type BenchmarkEpochFunc = Callable[[Database], list[str]]
 type BenchmarkSetupFunc = Callable[[Database], None]
