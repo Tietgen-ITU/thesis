714a5690357ea854d819c430190c62d3144ee3a4
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-28 05:19:13 +0000

    Fix run script

--- benchmark/duckdb/run.sh
+++ benchmark/duckdb/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-DURATION=20
+DURATION=60
 DEVICE="/dev/nvme1"
 INPUT_DIR="/home/pinar"
+MOUNT="/mnt/itu/duckdb"
 
+source /home/pinar/.bashrc
 source ./init.sh
 
 # Run the benchmark with the generic device, io_uring_cmd, and fdp
-python3 benchmark.py -d $DURATION --sf 1 --input_dir $INPUT_DIR --device_path $DEVICE --generic_device -b "io_uring_cmd" -m 80 --fdp tpch
+python3 benchmark.py -d $DURATION --input_directory $INPUT_DIR --device_path $DEVICE --generic_device -b "io_uring_cmd" -m 75 --fdp tpch
 
 # Run the benchmark with the generic device, io_uring_cmd, but without fdp
 # python3 benchmark.py -i $DURATION -d $DEVICE --generic_device -b "io_uring_cmd" tpch
@@ -15,4 +17,4 @@ python3 benchmark.py -d $DURATION --sf 1 --input_dir $INPUT_DIR --device_path $D
 # python3 benchmark.py -i $DURATION -d $DEVICE -b "io_uring" tpch
 
 # Run regular benchmark with default file system(could be a base line)
-# python3 benchmark.py -i $DURATION tpch
+python3 benchmark.py -d $DURATION --mount_path $MOUNT --input_directory $INPUT_DIR tpch
