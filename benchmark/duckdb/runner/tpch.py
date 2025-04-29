e12db78d1e93a306668048e1ea8083f38bb946cf
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-04-29 19:17:15 +0000

    Add db limitations after copy of db

--- benchmark/duckdb/runner/tpch.py
+++ benchmark/duckdb/runner/tpch.py
@@ -4,7 +4,7 @@ from database.duckdb import Database
 
 TPCH_BENCHMARK_NAME = "tpch"
 
-def setup_tpch_benchmark(db: Database, input_dir_path: str, scale_factor: int):
+def setup_tpch_benchmark(db: Database, input_dir_path: str, buffer_manager_size: int, scale_factor: int):
     input_file_path = os.path.join(input_dir_path, f"tpch-{scale_factor}.db")
 
     db.add_extension("tpch")
@@ -12,6 +12,9 @@ def setup_tpch_benchmark(db: Database, input_dir_path: str, scale_factor: int):
     db.query(f"ATTACH DATABASE '{input_file_path}' AS tpch (READ_WRITE);")
     db.query("COPY FROM DATABASE tpch TO bench;")
     db.query("DETACH DATABASE tpch;")
+    db.query(f"SET memory_limit='{buffer_manager_size}MB';")
+    db.query("SET threads=1;")
+    db.query("PRAGMA disable_object_cache;")
     # db.query("CALL dbgen(sf=1);")
 
 def run_tpch_epoch_benchmark(db: Database):
