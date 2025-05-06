bd1eeea0b7a6598f82f4e3366f05bd1136a3bc3a
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-05-06 20:08:36 +0000

    Set the default size of the device to 10%

--- benchmark/duckdb/device/nvme.py
+++ benchmark/duckdb/device/nvme.py
@@ -234,7 +234,7 @@ def calculate_waf(host_written_bytes, media_written_bytes):
 
     return media_written_bytes / host_written_bytes
 
-def setup_device(device: NvmeDevice, namespace_id:int = 1, enable_fdp: bool = False, mount_path: str = None) -> NvmeDeviceNamespace:
+def setup_device(device: NvmeDevice, namespace_id:int = 1, enable_fdp: bool = False, mount_path: str = None, size: float = 0.1) -> NvmeDeviceNamespace:
     """
     Sets up the device by creating a namespace and enabling FDP if required
     """
@@ -253,4 +253,4 @@ def setup_device(device: NvmeDevice, namespace_id:int = 1, enable_fdp: bool = Fa
         device.disable_fdp()
     
     # Create new namespace with a new configuration
-    return device.create_namespace(device.base_device_path, namespace_id, enable_fdp, mount_path=mount_path)
+    return device.create_namespace(device.base_device_path, namespace_id, enable_fdp, mount_path=mount_path, size=size)
