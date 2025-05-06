3ff14ae4a19aa7a287703666310da52d0a80be85
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-05-06 21:37:36 +0000

    Let the script precondition the device

--- benchmark/duckdb/device/nvme.py
+++ benchmark/duckdb/device/nvme.py
@@ -247,10 +247,10 @@ def setup_device(device: NvmeDevice, namespace_id:int = 1, enable_fdp: bool = Fa
         device.delete_namespace_nsid(namespace_id)
     
     # Ensure that FDP is enabled / disabled
-    if enable_fdp:
-        device.enable_fdp()
-    else:
-        device.disable_fdp()
+    # if enable_fdp:
+    #     device.enable_fdp()
+    # else:
+    #     device.disable_fdp()
     
     # Create new namespace with a new configuration
     return device.create_namespace(device.base_device_path, namespace_id, enable_fdp, mount_path=mount_path, size=size)
--- benchmark/duckdb/run.sh
+++ benchmark/duckdb/run.sh
@@ -10,14 +10,28 @@ M_SIZE_PRECONDITION=826334573 # ~3,38TB, 90% of device
 L_SIZE_DEVICE=156085420 # ~639GB, 17% of device
 L_SIZE_PRECONDITION=762064106 # ~3,12TB, 83% of device
 
-precondition_device() {
+precondition_device_fdp() {
     DEVICE_PATH=$1
     SIZE=$2
+
+    nvme set-feature $DEVICE_PATH -f 0x1D -c 1 -s
 
     nvme create-ns $DEVICE_PATH -b 4096 --nsze=$SIZE --ncap=$SIZE
     nvme attach-ns $DEVICE_PATH --namespace-id=1 --controllers=0x7
 
-    /home/pinar/.local/fio/fio --filename=/dev/ng1n1 --size="100%" --name fillDevice --rw=write --numjobs=1 --ioengine=io_uring_cmd --iodepth=64 --bs=128K
+    /home/pinar/.local/fio/fio --filename=/dev/ng1n1 --size="100%" --name fillDevice --rw=write --numjobs=1 --ioengine=io_uring_cmd --iodepth=64 --bs=256k
+}
+
+precondition_device() {
+    DEVICE_PATH=$1
+    SIZE=$2
+
+    nvme set-feature $DEVICE_PATH -f 0x1D -c 0 -s
+
+    nvme create-ns $DEVICE_PATH -b 4096 --nsze=$SIZE --ncap=$SIZE
+    nvme attach-ns $DEVICE_PATH --namespace-id=1 --controllers=0x7
+
+    /home/pinar/.local/fio/fio --filename=/dev/ng1n1 --size="100%" --name fillDevice --rw=write --numjobs=1 --ioengine=io_uring_cmd --iodepth=64 --bs=256k
 }
 
 remove_precondition_device() {
@@ -31,7 +45,7 @@ remove_precondition_device() {
 source /home/pinar/.bashrc
 source ./init.sh
 
-precondition_device $DEVICE $M_SIZE_PRECONDITION
+precondition_device_fdp $DEVICE $M_SIZE_PRECONDITION
 
 python3 benchmark.py -d $DURATION --input_directory $INPUT_DIR --device_path $DEVICE --generic_device -b "io_uring_cmd" -m 500 --sf 100 --fdp tpch
 
