87d04cbe406fcffb27adbbbcfe7f3636b03cc7c5
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-05-14 10:48:41 +0000

    Use sf prefix for tpch files

--- benchmark/duckdb/runner/tpch.py
+++ benchmark/duckdb/runner/tpch.py
@@ -5,7 +5,7 @@ from database.duckdb import Database
 TPCH_BENCHMARK_NAME = "tpch"
 
 def setup_tpch_benchmark(db: Database, input_dir_path: str, buffer_manager_size: int, scale_factor: int):
-    input_file_path = os.path.join(input_dir_path, f"tpch-{scale_factor}.db")
+    input_file_path = os.path.join(input_dir_path, f"tpch-sf{scale_factor}.db")
 
     db.add_extension("tpch")
 
