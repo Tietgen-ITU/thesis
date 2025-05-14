e6fdd21d9148b01f41f0d3289378feaacd4882e9
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-05-14 08:03:28 +0000

    Fix datageneration

--- benchmark/duckdb/scripts/oocha/datagen.py
+++ benchmark/duckdb/scripts/oocha/datagen.py
@@ -3,6 +3,7 @@ import os
 import duckdb
 
 temp_db = "temp.db"
+temp_csv = "temp.csv"
 scaling_factors = [2, 8, 32, 128]
 
 def generate_data(scale_factor: int, output_dir: str):
@@ -11,6 +12,9 @@ def generate_data(scale_factor: int, output_dir: str):
     """
 
     output_file = os.path.join(output_dir, f"oocha-{scale_factor}.db")
+
+    if os.path.exists(temp_csv):
+        os.remove(temp_csv)
 
     if os.path.exists(temp_db):
         os.remove(temp_db)
@@ -32,9 +36,14 @@ def generate_data(scale_factor: int, output_dir: str):
     # Generate the data using the dbgen function
     con.execute(f"CALL dbgen(sf={scale_factor});")
 
+    # Move data temporarily to CSV in order to only copy lineitem table
+    con.execute(f"COPY lineitem TO '{temp_csv}';")
+
     # Save the generated data to the output directory
     con.execute(f"ATTACH DATABASE '{output_file}' AS oocha (READ_WRITE);")
-    con.execute("COPY lineitem FROM DATABASE tempdatagen TO oocha;")
+    con.execute("USE oocha;")
+    con.execute("CREATE TABLE IF NOT EXISTS lineitem(l_orderkey INTEGER NOT NULL, l_partkey INTEGER NOT NULL, l_suppkey INTEGER NOT NULL, l_linenumber INTEGER NOT NULL, l_quantity DECIMAL(15,2) NOT NULL, l_extendedprice DECIMAL(15,2) NOT NULL, l_discount DECIMAL(15,2) NOT NULL, l_tax DECIMAL(15,2) NOT NULL, l_returnflag VARCHAR NOT NULL, l_linestatus VARCHAR NOT NULL, l_shipdate DATE NOT NULL, l_commitdate DATE NOT NULL, l_receiptdate DATE NOT NULL, l_shipinstruct VARCHAR NOT NULL, l_shipmode VARCHAR NOT NULL, l_comment VARCHAR NOT NULL);")
+    con.execute(f"""COPY lineitem FROM '{temp_csv}' (HEADER TRUE);""")
 
     # Close the connection
     con.close()
