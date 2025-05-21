c3477ed12b164e86c62747bf51cb4b01c4e58030
emiloh <82398520+emiloh@users.noreply.github.com>
2025-05-21 21:13:52 +0200

    fix: add source

--- benchmark/flamegraph/flame.sh
+++ benchmark/flamegraph/flame.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source /home/pinar/.bashrc
+source ./init.sh
+
 ./run.sh
 perf script | /home/pinar/.local/FlameGraph/stackcollapse-perf.pl > out.perf-folded 
 /home/pinar/.local/FlameGraph/flamegraph.pl out.perf-folded > /home/pinar/ucmd.svg
