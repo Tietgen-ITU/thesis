aa1eebe054313207a3b55f51d02a76032aedd602
emiloh <82398520+emiloh@users.noreply.github.com>
2025-05-26 14:22:09 +0200

    feat: add graphs from runs, update plot script to not include xnvme atm

--- benchmark/plot/plot.py
+++ benchmark/plot/plot.py
@@ -13,6 +13,11 @@ def get_single_result(measurement):
 
 def bytes_to_gb(value):
     return value / 1000000000
+def bytes_to_gib(value):
+    return value / 1073741824
+
+def to_coords(seq):
+    return map(lambda x: ((x[0]+1)*10, x[1]), enumerate(seq))
 
 def get_results(result_path_str):
     res = defaultdict(tuple)
@@ -32,6 +37,8 @@ def get_results(result_path_str):
             wafs = []
             hosts = []
             medias = []
+            hosts_gib = []
+            media_gib = []
 
             last = get_single_result(measurements[0]) 
             
@@ -44,11 +51,21 @@ def get_results(result_path_str):
                 wafs.append(next[0])
                 hosts.append(diff_host)
                 medias.append(diff_media)
+                hosts_gib.append(bytes_to_gib(diff_host))
+                media_gib.append(bytes_to_gib(diff_media))
                 last = next
+
+            with open(f"{result_path}/{name}_results.csv", mode="w+") as f:
+                f.write(" ".join(map(str, to_coords(wafs))))
+                f.write("\n")
+                f.write(" ".join(map(str, to_coords(hosts_gib))))
+                f.write("\n")
+                f.write(" ".join(map(str, to_coords(media_gib))))
+                f.write("\n")
             
             res[name] = (wafs, hosts, medias)
     
-    pairs = [("no_fdp", "fdp"), ("xnvme_no_fdp", "xnvme_fdp")]
+    pairs = [("no_fdp", "fdp")]#, ("xnvme_no_fdp", "xnvme_fdp")]
 
     for (nofdp, fdp) in pairs:
         if len(res[nofdp][0]) < len(res[fdp][0]):
@@ -70,7 +87,7 @@ def get_results(result_path_str):
 def plot_waf(result_path_str):
     results = get_results(result_path_str)
 
-    tests = [("no_xnvme", "no_fdp", "fdp"), ("xnvme", "xnvme_no_fdp", "xnvme_fdp")]
+    tests = [("no_xnvme", "no_fdp", "fdp")]#, ("xnvme", "xnvme_no_fdp", "xnvme_fdp")]
 
     for test in tests:
         no_fdp_y = results[test[1]][0]
@@ -78,14 +95,14 @@ def plot_waf(result_path_str):
         x_ticks = [x * 10 for x in range(1, len(no_fdp_y)+1)]
 
         fig, ax = plt.subplots()
-        ax.plot(x_ticks, no_fdp_y, label=test[1])
-        ax.plot(x_ticks, fdp_y, label=test[2])
+        ax.plot(x_ticks, no_fdp_y, label=test[1], color="r")
+        ax.plot(x_ticks, fdp_y, label=test[2], color="b")
 
         ax.set_ylim(bottom=1)
         ax.set_xlabel("Minutes")
         ax.set_ylabel("Write Amplification Factor (WAF)")
         ax.xaxis.set_major_locator(MultipleLocator(60))
-        ax.yaxis.set_major_locator(MultipleLocator(0.1))
+        ax.yaxis.set_major_locator(MultipleLocator(0.2))
         ax.legend(loc='upper left', ncols=1)
 
         plt.savefig(f"{result_path_str}/{test[0]}.pdf", format="pdf", bbox_inches="tight")
@@ -93,7 +110,7 @@ def plot_waf(result_path_str):
 def plot_write(result_path_str):
     results = get_results(result_path_str)
 
-    tests = [("no_xnvme", "no_fdp", "fdp"), ("xnvme", "xnvme_no_fdp", "xnvme_fdp")]
+    tests = [("no_xnvme", "no_fdp", "fdp")]#, ("xnvme", "xnvme_no_fdp", "xnvme_fdp")]
 
     for test in tests:
         no_fdp_host_y = list(map(bytes_to_gb, results[test[1]][1]))
@@ -103,15 +120,15 @@ def plot_write(result_path_str):
         x_ticks = [x * 10 for x in range(1, len(no_fdp_host_y)+1)]
 
         fig, ax = plt.subplots()
-        ax.plot(x_ticks, no_fdp_host_y, label=f"{test[1]}_host", color="r")
-        ax.plot(x_ticks, fdp_host_y, label=f"{test[2]}_host", color="b")
-        ax.plot(x_ticks, no_fdp_media_y, label=f"{test[1]}_media", linestyle="--", color="r")
-        ax.plot(x_ticks, fdp_media_y, label=f"{test[2]}_media", linestyle="--", color="b")
+        ax.plot(x_ticks, no_fdp_host_y, label=f"{test[1]}_host", linestyle="dotted", color="r")
+        ax.plot(x_ticks, fdp_host_y, label=f"{test[2]}_host", linestyle="dotted", color="b")
+        ax.plot(x_ticks, no_fdp_media_y, label=f"{test[1]}_media", linestyle="dashed", color="r")
+        ax.plot(x_ticks, fdp_media_y, label=f"{test[2]}_media", linestyle="dashed", color="b")
 
         ax.set_xlabel("Minutes")
         ax.set_ylabel("GBs written last 10 min")
-        ax.xaxis.set_major_locator(MultipleLocator(60))
-        ax.yaxis.set_major_locator(MultipleLocator(0.05))
+        ax.xaxis.set_major_locator(MultipleLocator(30))
+        ax.yaxis.set_major_locator(MultipleLocator(0.2))
         ax.legend(loc='upper left', ncols=1)
 
         plt.savefig(f"{result_path_str}/{test[0]}_write.pdf", format="pdf", bbox_inches="tight")
