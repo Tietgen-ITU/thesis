97147c85bb42dee3d8503b23c9e66ce3ffcac47b
emiloh <82398520+emiloh@users.noreply.github.com>
2025-05-30 16:49:25 +0200

    fixes

--- benchmark/duckdb/run_oocha_spdk.sh
+++ benchmark/duckdb/run_oocha_spdk.sh
@@ -1,7 +1,7 @@
 source "/home/pinar/.bashrc"
 source "./init.sh"
 
-cp -f "/home/pinar/copy_secrets/spdk_nvmefs.duckdb_secret" "root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
+cp -f "/home/pinar/copy_secrets/spdk_nvmefs.duckdb_secret" "/root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
 python3 ./benchmark_oocha_with_spdk.py "nvmefs://bench.db" 1 2000 "results/ocha_single_thread" 1
 
 cp -f "/home/pinar/copy_secrets/ucmd_nvmefs.duckdb_secret" "/root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
