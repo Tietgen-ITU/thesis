85ab0d67f1d6a7a32c8cdb1eee588b96d5caa997
emiloh <82398520+emiloh@users.noreply.github.com>
2025-06-02 04:30:16 +0200

    feat: oocha spdk benchmarks

--- benchmark/duckdb/run_oocha_spdk.sh
+++ benchmark/duckdb/run_oocha_spdk.sh
@@ -1,10 +1,10 @@
 source "/home/pinar/.bashrc"
 source "./init.sh"
 
-cp -f "/home/pinar/copy_secrets/spdk_nvmefs.duckdb_secret" "/root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
-python3 ./benchmark_oocha_with_spdk.py "nvmefs://bench.db" 1 128 "results/ocha_single_thread" 1
+#cp -f "/home/pinar/copy_secrets/spdk_nvmefs.duckdb_secret" "/root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
+#python3 ./benchmark_oocha_with_spdk.py "nvmefs://bench.db" 3 128 "results/ocha_single_thread" 1
 
-cp -f "/home/pinar/copy_secrets/ucmd_nvmefs.duckdb_secret" "/root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
-python3 ./benchmark_oocha_with_spdk.py "nvmefs://bench.db" 1 128 "results/ocha_single_thread" 0
+#cp -f "/home/pinar/copy_secrets/ucmd_nvmefs.duckdb_secret" "/root/.duckdb/stored_secrets/nvmefs.duckdb_secret"
+#python3 ./benchmark_oocha_with_spdk.py "nvmefs://bench.db" 3 128 "results/ocha_single_thread" 0
 
-python3 ./benchmark_oocha_with_spdk.py "bench.db" 1 128 "results/ocha_single_thread" 0
+python3 ./benchmark_oocha_with_spdk.py "/mnt/itu/duckdb/bench.db" 3 128 "results/ocha_single_thread" 0
