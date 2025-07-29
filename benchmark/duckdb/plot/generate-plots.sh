003779108587d21c23b57d35bac92e7f9d7065c3
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-07-29 20:02:21 +0200

    Use new input directory

--- benchmark/duckdb/plot/generate-plots.sh
+++ benchmark/duckdb/plot/generate-plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 plot_oocha.py ../results/shared_lock ./plots/oocha
-python3 plot_oocha_single.py ../results/ocha_single_thread ./plots/oocha/single_thread
-python3 plot_tpch.py ../results/May23 ./plots/tpch
-python3 plot_oocha-spill.py ../results/May23 ./plots/oocha/spill
+python3 plot_oocha.py ../results/output/oocha ./plots/oocha
+python3 plot_oocha_single.py ../results/output/oocha-single ./plots/oocha/single_thread
+python3 plot_tpch.py ../results/output/tpch ./plots/tpch
+python3 plot_oocha-spill.py ../results/output/oocha-spill ./plots/oocha/spill
