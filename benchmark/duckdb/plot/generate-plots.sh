36679117c0f0953532d95c8e94544940d0806710
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-07-29 20:05:45 +0200

    Move raw data to top of results directory

--- benchmark/duckdb/plot/generate-plots.sh
+++ benchmark/duckdb/plot/generate-plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 plot_oocha.py ../results/output/oocha ./plots/oocha
-python3 plot_oocha_single.py ../results/output/oocha-single ./plots/oocha/single_thread
-python3 plot_tpch.py ../results/output/tpch ./plots/tpch
-python3 plot_oocha-spill.py ../results/output/oocha-spill ./plots/oocha/spill
+python3 plot_oocha.py ../results/oocha ./plots/oocha
+python3 plot_oocha_single.py ../results/oocha-single ./plots/oocha/single_thread
+python3 plot_tpch.py ../results/tpch ./plots/tpch
+python3 plot_oocha-spill.py ../results/oocha-spill ./plots/oocha/spill
